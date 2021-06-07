eb978b1bab9751feea9dccfe5bd64e75376d3bff
Guy <Guy9502@Gmail.com>
2021-06-07 10:01:46 +0300

    Doing some cleaning.

--- Forum/src/Main.java
+++ Forum/src/Main.java
@@ -3,8 +3,7 @@ package com.github.forumproject;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("Test");
+		System.out.println("This is the main project! yay!");
 
 	}
 
